fb95fe6c776d2bea4d34733135edb0ef0e972ab4
Joe Jung <Joe Jung>
2017-11-18 00:23:41 -0500

    changed port number

--- src/main/java/com/vaya20/backend/config/SimpleCORSFilter.java
+++ src/main/java/com/vaya20/backend/config/SimpleCORSFilter.java
@@ -24,7 +24,7 @@ public class SimpleCORSFilter implements Filter {
 
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3080");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "false");
